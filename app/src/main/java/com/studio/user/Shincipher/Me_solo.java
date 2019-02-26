33c0a586b2b219d1880adb9c345a14f25ab3a4cd
Shincipher <watashigui98@gmail.com>
2019-02-26 12:21:37 +0700

    Initial commit

---
+++ app/src/main/java/com/studio/user/Shincipher/Me_solo.java
@@ -0,0 +1,18 @@
+package com.studio.user.Shincipher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Me_solo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_solo);
+
+    //        Add Fragment to activity
+        getSupportFragmentManager().beginTransaction().add(R.id.layoutMainFragmant, new MainFragment()).commit();
+
+    }   //Main Method
+    
+}    //Main Classs
